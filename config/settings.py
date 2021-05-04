d01a49c1e624a20bcdd745e4665633b4ddf4de38
df2017 <dferrey22783@gmail.com>
2021-05-04 09:36:33 -0300

    MODIFY: config deploy fixed

--- config/settings.py
+++ config/settings.py
@@ -99,12 +99,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {'default': dj_database_url.config()}
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
@@ -148,5 +142,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
